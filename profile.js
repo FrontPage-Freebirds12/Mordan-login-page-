66316397603212163463dc5178645b03cf048fb2
Dani Boii <105583437+Orta12@users.noreply.github.com>
2022-09-17 12:05:05 +0530

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,4 +1,4 @@
-function dhf(){
+function login(){
     var email = document.getElementById("email").value
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
